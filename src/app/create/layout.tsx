d4929c0baf3e7c198d7c9eadcbf195637096b1cb
Ryu jisu <wltn1418@gmail.com>
2023-08-17 19:47:50 +0900

    13-5: routing

---
+++ src/app/create/layout.tsx
@@ -0,0 +1,10 @@
+const layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <form action="">
+      <h2>Create layout</h2>
+      {children}
+    </form>
+  )
+}
+
+export default layout
---
+++ src/app/read/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const read = (props) => {
+  return (
+    <>
+    <div>read</div>
+    <h1>{props.params.id}</h1>
+    </>
+  )
+}
+
+export default read
