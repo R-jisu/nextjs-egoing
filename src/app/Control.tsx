62deb3a51b0a5d57e7bed3f99b55f6cc162630f5
Ryu jisu <wltn1418@gmail.com>
2023-08-21 20:41:10 +0900

    delete

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -1,16 +1,23 @@
 "use client"
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export function Control() {
   const params = useParams();
+  const router = useRouter();
   const id = params.id;
   return (
     <ul>
       <li><Link href="/create">Create</Link></li>
       {id ? <>
         <li><Link href={`/update/${id}`}>Update</Link></li>
-        <li><input type="button" value="delete" /></li>
+        <li><input type="button" value="delete" onClick={() => {
+          fetch(`http://localhost:9999/topics/${id}`, {method: 'DELETE'}).then(res => res.json).then(result => {
+          router.refresh();
+          router.push('/');
+          });
+        }} /></li>
       </> : null}
     </ul>
   );
